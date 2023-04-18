5140c0a91e85eefb824a2d8fc609b8f845b18924
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 10:46:56 +0300

    Initial commit

---
+++ GDTask-7/Program.cs
@@ -0,0 +1,28 @@
+﻿foreach (var el in schitalka(20))
+{
+    Console.Write($"{el} ");
+}
+static int[] schitalka(int num)
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    for (int i = 1; i < num; i++)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            a++;
+        }
+        else if (i % 5 == 0 && i % 3 != 0)
+        {
+            b++;
+        }
+        else if (i % 3 == 0 && i % 5 == 0)
+        {
+            c++;
+        }
+    }
+    
+    return new[] { a, b, c };
+}
